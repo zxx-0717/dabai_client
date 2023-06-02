a58810851851ffea75473739e47b985451320357
xiaobin0630 <wangxiaobin_hfut@163.com>
2023-06-02 14:42:20 +0800

    add pub  person_detected msg

--- src/dabai_client/dabai_client/nanodet_client.py
+++ src/dabai_client/dabai_client/nanodet_client.py
@@ -3,28 +3,35 @@ import cv2
 import time
 import socket
 from .utils import *
-
+from struct import pack,unpack
 
 import rclpy
 import cv2
 from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import CameraInfo,PointCloud2
 from rclpy.qos import qos_profile_sensor_data
+from capella_ros_msg.msg import DetectResult
+from capella_ros_msg.msg import SingleDetector
+
 
 class DaBaiSubscriber(Node):
 
         def __init__(self):
                 super().__init__('DaBai_Subscriber')
+                self.depth_data = []
                 self.color_subscription = self.create_subscription(Image,'/camera/color/image_raw',
                 self.color_subscription_callback,qos_profile_sensor_data)
                 self.color_subscription  # prevent unused variable warning
 
-                self.depth_subscription = self.create_subscription(Image,'/camera/depth/image_raw',
+                self.depth_subscription = self.create_subscription(PointCloud2,'/camera/depth/points',
                 self.depth_subscription_callback,
                 qos_profile_sensor_data)
                 self.depth_subscription  # prevent unused variable warning
+
+                self.pub_pose = self.create_publisher(DetectResult, '/person_detected', 2)
+
 
                 # 服务端ip地址
                 HOST = '192.168.180.8'
@@ -68,37 +75,96 @@ class DaBaiSubscriber(Node):
                 data = self.tcpClient.recv(1024)
 
                 result_ = np.fromstring(data).reshape((-1,6))
-                print(result_)
+                # print("延时：" + str(int((time.perf_counter() - start) * 1000)) + "ms")
+                # print(result_)
                 det_bboxes, det_conf, det_classid = result_[:,:4],result_[:,4],result_[:,5].astype(int)
+                det_bboxes = det_bboxes[det_classid == 0]
+                det_conf = det_conf[det_classid == 0]
+                det_classid = det_classid[det_classid == 0]
+                msg = DetectResult()
+                
+                if len(det_classid) > 0 and len(self.depth_data) > 0:
+                        self.srcimg = img_draw(self.srcimg,det_bboxes, det_conf, det_classid,newh, neww, top, left)
+                        center_point_x = (det_bboxes[:,2] - det_bboxes[:,0]).reshape((-1,1)).astype(int)
+                        center_point_y = (det_bboxes[:,3]-det_bboxes[:,1]).reshape((-1,1)).astype(int)
+                        center_point = np.concatenate([center_point_x,center_point_y],axis=1)
+                        # print(center_point_x)
+                        # print(center_point_y)
+                        # center_point = np.array([[240,320]])
+                        print(center_point)
+                        
+                        for x,y in center_point:
+                                single_msg = SingleDetector() 
+                                x_ = self.depth_data[y][x][:4]
+                                y_ = self.depth_data[y][x][4:8]
+                                z_ = self.depth_data[y][x][8:12]
+                                depth_x = unpack("<f",pack('4B',*x_))[0]
+                                depth_y = unpack("<f",pack('4B',*y_))[0]
+                                depth_z = unpack("<f",pack('4B',*z_))[0]
+                                print(depth_x,depth_y,depth_z)
+                                print(type(depth_z))
+                                if np.isnan(depth_z):
+                                        single_msg.part = False
+                                        single_msg.x = 100.
+                                        single_msg.y = 100.
+                                        single_msg.z = 100.
+                                        msg.result.append(single_msg) 
+                                else: 
+                                        single_msg.part = False
+                                        single_msg.x = depth_z
+                                        single_msg.y = depth_y
+                                        single_msg.z = depth_x 
+                                        msg.result.append(single_msg)                             
+                else:
+                        single_msg = SingleDetector() 
+                        single_msg.part = False
+                        single_msg.x = 100.
+                        single_msg.y = 100.
+                        single_msg.z = 100.
+                        msg.result.append(single_msg) 
+                self.pub_pose.publish(msg)
+
                 # print(det_bboxes)
                 # print(det_conf)
                 # print(det_classid)
-                result_img = img_draw(self.srcimg,det_bboxes, det_conf, det_classid,newh, neww, top, left)
                 # cv2.imshow('result',result_img)
                 # cv2.waitKey(0)
                 # cv2.destroyAllwindows()
                 # cv2.imwrite(r'/workspaces/rknn-toolkit/nanodet-client/img3_result.jpg',result_img)
                 #     # 计算发送完成的延时
-                print("延时：" + str(int((time.perf_counter() - start) * 1000)) + "ms")
+                
 
 
                 # self.get_logger().info('color_height: "%s"' % self.color_height)
                 # self.get_logger().info('color_width: "%s"' % self.color_width)
                 # self.get_logger().info('data: "%s"' % self.data)
-                cv2.imshow('img',result_img)
+                cv2.imshow('img',self.srcimg)
+                # cv2.imshow('img',self.srcimg)
                 cv2.waitKey(1)
 
         def depth_subscription_callback(self, msg):
                 self.depth_width = msg.width
                 self.depth_height = msg.height
-                self.depth_data = msg.data
-                self.color_data = np.array(msg.data).reshape((self.depth_height,self.depth_width,2))[:,:,0]
+                self.depth_data = np.array(msg.data).reshape((480,640,-1)).astype(int)
+                # print(self.depth_data.shape)
+                # self.color_data = np.array(msg.data).reshape((self.depth_height,self.depth_width,2))[:,:,0]
 
                 # self.get_logger().info('depth height: "%s"' % self.depth_height)
                 # self.get_logger().info('depth width: "%s"' % self.depth_width)
-                # self.get_logger().info('depth data: "%s"' % self.color_data)
+                # self.get_logger().info('depth data: "%s"' % self.depth_data[:16])
                 # cv2.imshow('depth0',self.color_data)
                 # cv2.waitKey(1)
+
+                # center_point = np.array([[240,320]])
+                # # print(center_point)
+                # for x,y in center_point:
+                #         x_ = self.depth_data[y][x][:4]
+                #         y_ = self.depth_data[y][x][4:8]
+                #         z_ = self.depth_data[y][x][8:12]
+                #         depth_x = unpack("<f",pack('4B',*x_))[0]
+                #         depth_y = unpack("<f",pack('4B',*y_))[0]
+                #         depth_z = unpack("<f",pack('4B',*z_))[0]
+                #         print(depth_x,depth_y,depth_z)
 
 
 
